b053992efd0c8453e14ada9f59df03d666d28465
Bảo Long <anhruoia1a1@gmail.com>
2025-11-02 15:15:46 +0700

    update reaction for post and comment

--- Domain/Entities/CommentReactionUser.cs
+++ Domain/Entities/CommentReactionUser.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Domain.Entities
@@ -11,7 +12,9 @@ namespace Domain.Entities
         public Guid UserId { get; set; }
         public User? User { get; set; }
         public Guid CommentId { get; set; }
+        [JsonIgnore]
         public Comment? Comment { get; set; }
+        public required string Reaction { get; set; }
         public required DateTime CreatedAt { get; set; }
         public required DateTime UpdatedAt { get; set; }
     }
--- Domain/Entities/PostReactionUser.cs
+++ Domain/Entities/PostReactionUser.cs
@@ -1,11 +1,15 @@
-﻿namespace Domain.Entities
+﻿using System.Text.Json.Serialization;
+
+namespace Domain.Entities
 {
     public class PostReactionUser
     {
         public Guid UserId { get; set; }
         public User? User { get; set; }
         public Guid PostId { get; set; }
+        [JsonIgnore]
         public Post? Post { get; set; }
+        public required string Reaction { get; set; }
         public required DateTime CreatedAt { get; set; }
         public required DateTime UpdatedAt { get; set; }
     }
